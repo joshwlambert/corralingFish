c4b72c40ba60c60db805889050d2973996c1fa24
Neves-P <p.m.santos.neves@rug.nl>
2020-09-25 14:57:27 +0200

    Standardize species names

--- scripts/plotting_traits_fam.R
+++ scripts/plotting_traits_fam.R
@@ -102,11 +102,9 @@ reef_scom <- reef_scom_data$species_SFTEP
 #however, created trimmed vectors with these values, in case they will be useful later
 
 all_serran <- unlist(all_fam[["Serranidae"]])
-all_serran <- gsub(" ", "_", all_serran)
 mol_phy_all_serran <- fishtree_phylogeny(species = all_serran)
 #only pulls out 218 species of 549
 trimmed_serran <- mol_phy_all_serran[["tip.label"]]
-all_serran %in% trimmed_serran
 
 all_carran <- unlist(all_fam[["Carangidae"]])
 mol_phy_all_caran <- fishtree_phylogeny(species = all_carran)
@@ -139,11 +137,9 @@ mol_phy_all_haem <- fishtree_phylogeny(species = all_haem)
 trimmed_haem <- mol_phy_all_haem[["tip.label"]]
 
 all_poma <- unlist(all_fam[["Pomacentridae"]])
-all_poma <- gsub(" ", "_", all_poma)
 mol_phy_all_poma <- fishtree_phylogeny(species = all_poma)
 #only pulls 222 of 400 species
 trimmed_poma <- mol_phy_all_poma[["tip.label"]]
-all_poma %in% trimmed_poma
 
 all_tetra <- unlist(all_fam[["Tetraodontidae"]])
 mol_phy_all_tetra <- fishtree_phylogeny(species = all_tetra)
@@ -167,6 +163,7 @@ plot(mol_phy_all_serran, show.tip.label = FALSE, no.margin = TRUE)
 #categorise species as reef associated
 reef_serran_trait_data <- data.frame(trimmed_serran, row.names = trimmed_serran)
 reef_serran_trait_data$trait <- rep("non_reef", length(reef_serran_trait_data$trimmed_serran))
+reef_serran <- gsub(x = reef_serran, pattern = " ", replacement = "_")
 reef_serran_trait_data[which(reef_serran_trait_data$trimmed_serran %in% reef_serran), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -186,6 +183,7 @@ plot(mol_phy_all_caran, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_caran_trait_data <- data.frame(trimmed_caran, row.names = trimmed_caran)
 reef_caran_trait_data$trait <- rep("non_reef", length(reef_caran_trait_data$trimmed_caran))
+reef_caran <- gsub(x = reef_caran, pattern = " ", replacement = "_")
 reef_caran_trait_data[which(reef_caran_trait_data$trimmed_caran %in% reef_caran), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -201,6 +199,7 @@ plot(mol_phy_all_mura, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_mura_trait_data <- data.frame(trimmed_mura, row.names = trimmed_mura)
 reef_mura_trait_data$trait <- rep("non_reef", length(reef_mura_trait_data$trimmed_mura))
+reef_mura <- gsub(x = reef_mura, pattern = " ", replacement = "_")
 reef_mura_trait_data[which(reef_mura_trait_data$retrimmed_mura %in% reef_mura), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -217,6 +216,7 @@ plot(mol_phy_all_labri, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_labri_trait_data <- data.frame(trimmed_labri, row.names = trimmed_labri)
 reef_labri_trait_data$trait <- rep("non_reef", length(reef_labri_trait_data$trimmed_labri))
+reef_labri <- gsub(x = reef_labri, pattern = " ", replacement = "_")
 reef_labri_trait_data[which(reef_labri_trait_data$trimmed_labri %in% reef_labri), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -232,6 +232,7 @@ plot(mol_phy_all_exo, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_exo_trait_data <- data.frame(trimmed_exo, row.names = trimmed_exo)
 reef_exo_trait_data$trait <- rep("non_reef", length(reef_exo_trait_data$trimmed_exo))
+reef_exo <- gsub(x = reef_exo, pattern = " ", replacement = "_")
 reef_exo_trait_data[which(reef_exo_trait_data$trimmed_exo %in% reef_exo), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -247,6 +248,7 @@ plot(mol_phy_all_scorp, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_scorp_trait_data <- data.frame(trimmed_scorp, row.names = trimmed_scorp)
 reef_scorp_trait_data$trait <- rep("non_reef", length(reef_scorp_trait_data$trimmed_scorp))
+reef_scorp <- gsub(x = reef_scorp, pattern = " ", replacement = "_")
 reef_scorp_trait_data[which(reef_scorp_trait_data$trimmed_scorp %in% reef_scorp), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -262,6 +264,7 @@ plot(mol_phy_all_haem, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_haem_trait_data <- data.frame(trimmed_haem, row.names = trimmed_haem)
 reef_haem_trait_data$trait <- rep("non_reef", length(reef_haem_trait_data$trimmed_haem))
+reef_haem <- gsub(x = reef_haem, pattern = " ", replacement = "_")
 reef_haem_trait_data[which(reef_haem_trait_data$trimmed_haem %in% reef_haem), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -277,6 +280,7 @@ plot(mol_phy_all_poma, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_poma_trait_data <- data.frame(trimmed_poma, row.names = trimmed_poma)
 reef_poma_trait_data$trait <- rep("non_reef", length(reef_poma_trait_data$trimmed_poma))
+reef_poma <- gsub(x = reef_poma, pattern = " ", replacement = "_")
 reef_poma_trait_data[which(reef_poma_trait_data$trimmed_poma %in% reef_poma), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -292,6 +296,7 @@ plot(mol_phy_all_tetra, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_tetra_trait_data <- data.frame(trimmed_tetra, row.names = trimmed_tetra)
 reef_tetra_trait_data$trait <- rep("non_reef", length(reef_tetra_trait_data$trimmed_tetra))
+reef_tetra <- gsub(x = reef_tetra, pattern = " ", replacement = "_")
 reef_tetra_trait_data[which(reef_tetra_trait_data$trimmed_tetra %in% reef_tetra), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -307,6 +312,7 @@ plot(mol_phy_all_ophich, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_ophich_trait_data <- data.frame(trimmed_ophich)
 reef_ophich_trait_data$trait <- rep("non_reef", length(reef_ophich_trait_data$trimmed_ophich))
+reef_ophich <- gsub(x = reef_ophich, pattern = " ", replacement = "_")
 reef_ophich_trait_data[which(reef_ophich_trait_data$trimmed_ophich %in% reef_ophich), 2] <- "reef"
 
 #plot traits on phylogeny
@@ -322,6 +328,7 @@ plot(mol_phy_all_scom, show.tip.label = FALSE)
 #categorise species as reef associated
 reef_scom_trait_data <- data.frame(trimmed_scom, row.names = trimmed_scom)
 reef_scom_trait_data$trait <- rep("non_reef", length(reef_scom_trait_data$trimmed_scom))
+reef_scom <- gsub(x = reef_scom, pattern = " ", replacement = "_")
 reef_scom_trait_data[which(reef_scom_trait_data$trimmed_scom %in% reef_scom), 2] <- "reef"
 
 #plot traits on phylogeny
