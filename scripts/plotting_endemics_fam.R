c60232bd1c5c78b8c1854b20bff9f0a3f923515b
Emily <eedavis3@illinois.edu>
2020-10-02 16:02:20 +0200

    more streamlined code that plots endemicity of reef fish as binary trait. The phylogeny plot sizes need to be toggled a bit based on the number of species in the tree

--- scripts/plotting_endemics_fam.R
+++ scripts/plotting_endemics_fam.R
@@ -1,4 +1,4 @@
-#plot a tree with a binary trait at the tips to represent galapagos endemics
+#plot a tree with a binary trait at the tips to represent reef fish endemics
 
 ################load libraries
 library(fishtree)
@@ -10,9 +10,10 @@ library(phytools)
 
 
 ##################load data
+
 reef_fish_data <- read.csv("../GalapagosFishes_AA (2).csv")
 
-################## create a family list of endemics
+################create a family list of endemics and get all the names
 
 family_endlist <- c("Atherinopsidae", "Bythitidae", "Chaenopsidae", "Chlopsidae",
                     "Clupeidae", "Dactyloscopidae", "Gobiidae", "Haemulidae",
@@ -27,20 +28,78 @@ for (i in family_endlist) {
   all_famend[[i]] <- rfishbase::species_list(Family = i )
 }
 
-####################extract species from OG csv into a dataframe
-####################with only endemic species
+#########extract species from OG csv into a dataframe with only endemic species
 
 gala_endemic_data <-reef_fish_data %>%
     dplyr::filter( Family %in% family_endlist & Galapagos_Only == 1 )
 
-# gala_endemic <- list()
-#
-# for (i in family_endlist) {
-#   gala_end[[i]] <-
-#   }
+##########extract just the names of the species endemic per family
+reef_all <- list()
+reef_famall <- list()
 
-############################extract moe
+for ( i in family_endlist) {
 
+  reef_all[[i]] <- gala_endemic_data[which(gala_endemic_data$Family == i),]
+  reef_famall[[i]] <- reef_all[[i]][,5]
+  reef_famall[[i]] <- gsub(x = reef_famall[[i]] , pattern = " ", replacement = "_")
+}
+
+##############extract molecular phylogeny of species
+
+all_mol_trees <- list()
+
+for (i in family_endlist) {
+  all_mol_trees[[i]] <-
+    fishtree::fishtree_phylogeny(species = (unlist(all_famend[[i]])))
+  }
+
+###########create trimmed species lists based on molecular tree data
+
+trimmed_mol_trees <- list()
+
+for ( i in family_endlist) {
+  trimmed_mol_trees[[i]] <- all_mol_trees[[i]]["tip.label"]
+}
+
+#############split out the families, and categorise as reef associated within
+
+endemic_trait_data <- list()
+
+
+for (i in family_endlist) {
+  endemic_trait_data[[i]] <- data.frame(trimmed_mol_trees[[i]])
+  endemic_trait_data[[i]]["trait"] <- rep("nonendemic",
+                                          length(endemic_trait_data[[i]]))
+  endemic_trait_data[[i]][which(endemic_trait_data[[i]][["tip.label"]]
+                                %in% reef_famall[[i]]), 2] <- "endemic"
+  #endemic_trait_data[[i]] <- row.names(endemic_trait_data[[i]]["tip.label"])
+}
+
+############ create fmode vectors?
+
+fmode_all <- list()
+
+for (i in family_endlist) {
+
+  fmode_all[[i]] <- as.factor(setNames(endemic_trait_data[[i]]$trait,
+                                   endemic_trait_data[[i]]$tip.label))
+}
+
+########## create dotTree for each family, and save output at pdf file
+
+for (i in family_endlist) {
+
+  dotTree(all_mol_trees[[i]], fmode_all[[i]],
+          colors=setNames(c("blue", "red"), c("nonendemic", "endemic")),
+          ftype= "i", fsize = 0.1, no.margin = TRUE)
+  legend(x = 5 , legend = i )
+  }
+
+
+#how to actually assign names to vectors in a foor loop
+# for (i in 1:10) {
+# print(paste("fmode_", i, sep = ""))
+# }
 
 
 
