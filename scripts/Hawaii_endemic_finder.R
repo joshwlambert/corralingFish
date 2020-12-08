bf8b56de2e185e7d34a3e2b21ed7da4d178c1bd4
Emily <eedavis3@illinois.edu>
2020-12-08 10:43:46 -0500

    added code to get boundary box

--- scripts/Hawaii_endemic_finder.R
+++ scripts/Hawaii_endemic_finder.R
@@ -5,7 +5,34 @@
 #   2: list of species
 #   3: range data for list of species
 
-#get boundary box for the Hawaiian archipelago
+#get boundary box for the Hawaiian archipelago (from map_stuff.R scipt xo Pratik)
+NOAA <- read.csv("./Friedlander_data/Hawaii_NOAA_fish_data.csv")
+sample_points = distinct(NOAA, Lat, Long, Island, Region)
+  #get data
+states = sf::st_read("ne_50m_admin_1_states_provinces/")
+hwi = dplyr::filter(states, name_id == "Hawaii")
+  # make sample points sf
+sample_points_sf = sf::st_as_sf(sample_points,
+                                coords = c("Long", "Lat"))
+  # check crs
+st_crs(hwi)
+st_crs(sample_points_sf)
+  # set crs
+st_crs(sample_points_sf) = st_crs(hwi)
+  # transform data to 2782
+sample_pts_utm = st_transform(sample_points_sf, 2782)
+  # flatten the layer
+sample_pts_utm = st_combine(sample_pts_utm)
+  #create bounding box
+boun_box <- st_bbox(sample_points_sf)
+  #plot with all sampling points and boundary box
+ggplot()+
+  geom_sf(data = sample_points_sf,
+          colour = "red",
+          size = 0.2)+
+  coord_sf(xlim = c(-178.3843, -154.8066),
+           ylim = c(18.9192, 28.4595))+
+  theme_bw()
 
 #try with two different lists: molecular tree list and non-molecular tree list
 fish_data <- read.csv("./trimmed_NOAA_taxa.csv")
