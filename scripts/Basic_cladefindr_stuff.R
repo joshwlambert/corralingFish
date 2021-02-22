82b78a766340e0be167d099121b4c32c4e1fe5b8
Emily <eedavis3@illinois.edu>
2021-02-22 12:58:54 +0100

    put together all of sources scripts to create cladeR function

--- scripts/Basic_cladefindr_stuff.R
+++ scripts/Basic_cladefindr_stuff.R
@@ -2,8 +2,11 @@
 library(dplyr)
 library(fishtree)
 library(tibble)
+library(phylobase)
+library(tidyverse)
+library(stringr)
 
-##read in dataset
+#######---------read in dataset and filter for molecular data and depth-----####
 fishframe <- read.csv("../datasets/full_dataset_wmissingsp_moleculartwoo.csv")
 molecular_fishframe <- fishframe %>% filter(molecular_data == 1)
 molecular_fishframe <- fishframe %>% filter(molecular_data == 1, depth_lower <= 150)
@@ -14,26 +17,10 @@ clades_data <- clade_data %>% remove_rownames() %>%
 View(clades_data)
 
 
-#create dataframe to estimate number of species in a Family and/or Genera
-#present on Hawaii
-
-# n_genera <- fishframe %>% arrange(Family) %>% group_by(Family) %>% count(Genera)
 
 
-# create subset datasets based on the number of species in each genera
-  # i.e. "this dataframe contains all Hawaiian genera with two species on the island
-
-  #or, could begin each branch by creating df, with the conditional n_genera$n == ?
-
-
-#load in tree to use: this function returns either the single molecular object,
-#or a randomly selected large tree, which you select with character "molecular"
-# or character "complete"
-
-#loads in tree, based on type of tree you would like
-  #should look how to develop this later for used the whole arrary of 100
-  #complete trees, so that it can be used later.
-
+cladeR <- function(df_clades, treetype, island_age){
+#######----------pull phylogeny you want to use -------##########
 #assign outside of the function to not have to use global assignment
 #options are either "molecular" or "complete"
 get_tree <- function(tree_type){
@@ -50,50 +37,31 @@ get_tree <- function(tree_type){
   }
 }
 
-get_tree(tree_type = "molecular")
-#input into function, taxa with attached endemicitt/missing species status
-#attach species to phylo tree, with data, four types:
-#   1. not on hawaii
-#   2. hawaii missing species   ---- > might not be nescessary if using only
-#       molecular dataset, because no missing species will show up :(
-#   3. Hawaii nonendemic
-#   4. Hawaii endemic
-
-#create a phylo4 object, and add in data?
-names(tree)
-
+tree <- get_tree(tree_type = treetype)
+###--------Create combined tree object-----####
 g1_tree <- as(tree, "phylo4")
-#data is loaded in as clade
-View(clades_data)
-
 #combine to create a phylo4d object. by setting missing data to "warn" should set
 #any species not in the dataset e.g. #!1 to have an NA values
+phylo <- phylo4d(g1_tree, df_clades, missing.data = "warn")
 
-g2 <- phylo4d(g1_tree, clades_data, missing.data = "warn")
-
-# this just gets ride of all other species in the phylo object using phylobase
-g3 <- match.phylo.data(tree, clades_data)
-
-#trying a different phylo4d method
-
-g4 <- phylo4d(tree, tip.data =clade_data)
-
-which(g2@label == "Abudefduf_abdominalis")
-g2@label[2152]
-g2@data["2152",]
-
-
-#finds shortest path between two taxa
-shortestPath(g2, "Abudefduf_abdominalis", "Abudefduf_sordidus")
-
-#find out if a species has siblings
-siblings(g2, 2152, include.self = TRUE)
-
-#find out ancestors
-ancestor(g2,2152)
-
-
+#######-----------write general function to extract branching times ----#####
 get_leaf <- function(phy4object, leaf_name) {
   the_node <- which(phy4object@label == leaf_name)
   return(unname(edgeLength(phy4object)[getEdge(phy4object, the_node)]))
 }
+##########-------running extraction functions for non endemics/endemics----####
+source("../corralingFish/scripts/extracting_endemics.R")
+source("../corralingFish/scripts/extracting_nonendemics.R")
+  output_nonendemics <- get_nonendemics(phy4object = phylo, island_max_Age = island_age)
+  output_endemics <- get_endemics(phy4object = phylo , island_max_Age = islange_age)
+#######-------merge dataframes to create final full dataframe-------########
+island_df <- full_join(output_nonendemics, output_endemics, by = c("Taxa", "branching_times", "status"))
+  Missing_species <- c(rep(0, length(island_df$Ta)))
+island_df <- tibble::add_column(island_df, Missing_species = rep(0, length(island_df$Taxa)), .before = "status")
+
+print("dataframe output is in DAISIE input format")
+return(island_df)
+}
+
+
+output <- cladeR(clades_data, "molecular", 28)
